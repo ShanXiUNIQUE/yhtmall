a534cad822985f1e9fdbcec750731dbba8e24bd0
zhangkaifengs <1064486551@qq.com>
2016-07-30 08:39:41 +0800

    wancheng

---
+++ js/zkf-newlist.js
@@ -0,0 +1,17 @@
+for(var i=0;i<$('.zkfBotItem').length;i++)
+{
+	if(i%2)
+	{
+		$('.zkfBotItem').eq(i).css({
+			margin:'19px 0 0 0'
+		})
+	}else
+	{
+		$('.zkfBotItem').eq(i).css({
+			margin:'19px 19px  0 0'
+		})
+	}
+}
+
+
+	
